e2e48ccf179cc7905e1b281e48e7e26ca4902651
lilyeggy <2217386257@qq.com>
2025-01-25 11:46:42 +0800

    initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++learning
+//
+//  Created by SHYN on 2025/1/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
